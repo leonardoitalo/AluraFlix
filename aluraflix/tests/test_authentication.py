3643ff033f2143d464f769d1e11a840f3d0ae0e6
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-01-08 20:07:30 -0300

    Criando testepara autenticar usuario

---
+++ aluraflix/tests/test_authentication.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from django.contrib.auth import authenticate
+
+
+class AuthenticationUserTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user("c3po", password="123456")
+
+
+    def test_autenticacao_user_com_credenciais_corretas(self):
+        """Teste que verifica a autenticacao de um user com as credenciais corretas"""
+        user = authenticate(username="c3po", password="123456")
+        self.assertTrue((user is not None) and user.is_authenticated)
